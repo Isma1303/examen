821c5959c40b6f13d022df4cb89887c7ba3ee786
Isma1303 <liquezismael@gmail.com>
2024-06-08 16:12:07 -0600

    Arreglos #6

--- src/index.ts
+++ src/index.ts
@@ -7,28 +7,28 @@ import { CrearFactura, eliminarfactura, obtenerFacturasPorCliente, ActualizarFac
 import { CrearReceta, eliminarReceta } from "./interfaces/mantenimiento_interface/MantenimientoRecetas";
 // import { actualizarProductoS } from "./interfaces/mantenimiento_interface/MantenimientoDeProductos";
 import { ActualizarCita } from "./interfaces/cita.interface";
-crearUsuario({nombre:'Rene', carnet: 12245, clave:'rene123', habilitado: false})
-eliminarUsuario(3)
-actualizarUsuario(2,{nombre: 'Isma'})
-obtenerEdadPaciente(1)
+// crearUsuario({nombre:'Rene', carnet: 12245, clave:'rene123', habilitado: false})
+// eliminarUsuario(3)
+// actualizarUsuario(2,{nombre: 'Isma'})
+// obtenerEdadPaciente(1)
 
 
-MantenimientoDeProductos.CrearServicio({id: 3, tipo:'servicio', nombre: 'Limpueza bucal', precio:100,stock:true})
+// MantenimientoDeProductos.CrearServicio({id: 3, tipo:'servicio', nombre: 'Limpueza bucal', precio:100,stock:true})
 // MantenimientoDeProductos.actualizarProductoS(3,{stock:false})
-MantenimientoDeProductos.eliminarServicio(3)
+// MantenimientoDeProductos.eliminarServicio(1)
 // MantenimientoDeProductos.buscarproducto(1)
 
-CrearReceta({recetaId : 4, doctorId : 1, pacienteId:2,fecha:new Date('10-12-2024'),medicamentos:[]})
-eliminarReceta(1)
+// CrearReceta({recetaId : 4, doctorId : 1, pacienteId:2,fecha:new Date('10-12-2024'),medicamentos:[]})
+// eliminarReceta(1)
 
-CrearFactura({facturaId:4, clienteId:1,fecha: new Date("10-10-2024"),serviciosConsumidos:[], total:120})
+// CrearFactura({facturaId:4, clienteId:1,fecha: new Date("10-10-2024"),serviciosConsumidos:[], total:120})
 // eliminarfactura(1)
 // obtenerFacturasPorCliente(2)
 // ActualizarFactura(1,{} )
 
 
-cancelarCita(1)
-programarCita(4, 1,"12-12-2024","El paciente necesita hacer limpieza general")
+// cancelarCita(1)
+// programarCita(4, 1,"12-12-2024","El paciente necesita hacer limpieza general")
 // obtenerCitasDoctor(2)
 // obtenerCitasPaciente(1)
 // obtenerCitasPorFecha("12-12-2024")
