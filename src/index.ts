2f022f52f39da06f88c08cad741298f3e400e2ca
Isma1303 <liquezismael@gmail.com>
2024-06-09 02:34:25 -0300

    Arreglossss

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import * as MantenimientoDeProductos from "./interfaces/mantenimiento_interface/
 import { crearUsuario,actualizarUsuario,eliminarUsuario } from "./interfaces/mantenimiento_interface/MantenimientoUsuarios";
 import { cancelarCita, obtenerCitasDoctor, obtenerCitasPaciente, obtenerCitasPorFecha, programarCita } from "./interfaces/mantenimiento_interface/MantenimientoCitas";
 import { MantenimientoFactura } from "./interfaces/mantenimiento_interface/MantenimientoFactura";
-import { CrearReceta, eliminarReceta } from "./interfaces/mantenimiento_interface/MantenimientoRecetas";
+// import { cargarRecetas } from "./interfaces/mantenimiento_interface/MantenimientoRecetas";
 import { actualizarProductoS } from "./interfaces/mantenimiento_interface/MantenimientoDeProductos";
 import { Receta } from "./interfaces/receta.interface";
 import { MantenimientoPacientes } from "./interfaces/mantenimiento_interface/MantenimientoPacientes";
@@ -11,38 +11,16 @@ import { ActualizarCita } from "./interfaces/cita.interface";
 import { obtenerCitas } from "./interfaces/mantenimiento_interface/MantenimientoCitasDC";
 import {  DoctoR } from "./interfaces/mantenimiento_interface/MantenimientoDisponibilidadDeDoctor";
 import { obtenerEdadPaciente } from "./interfaces/mantenimiento_interface/MantenimientoDeProductos";
-// crearUsuario({nombre:'Rene', carnet: 12245, clave:'rene123', habilitado: false})
-// eliminarUsuario(3)
-// actualizarUsuario(2,{nombre: 'Isma'})
-// obtenerEdadPaciente(1)
+const paciente = new MantenimientoPacientes();
 
-
-// MantenimientoDeProductos.CrearServicio({id: 3, tipo:'servicio', nombre: 'Limpueza bucal', precio:100,stock:true})
-// MantenimientoDeProductos.actualizarProductoS(1,{stock:false})
-// MantenimientoDeProductos.eliminarServicio(1)
-// MantenimientoDeProductos.buscarproducto(2)
-
-// CrearReceta({recetaId : 4, doctorId : 1, pacienteId:2,fecha:new Date('10-12-2024'),medicamentos:[]})
-// eliminarReceta(1)
-
-// CrearFactura({facturaId:4, clienteId:1,fecha: new Date("10-10-2024"),serviciosConsumidos:[], total:120})
-// eliminarfactura(1)
-// obtenerFacturasPorCliente(2)
-// ActualizarFactura(2,{clienteId: 3} )
-
-
-// cancelarCita(1)
-// programarCita(4, 1,"12-12-2024","El paciente necesita hacer limpieza general")
-// obtenerCitasDoctor(2)
-// obtenerCitasPaciente(2)
-// obtenerCitasPorFecha("12-12-2024")
 
 
 /*edad del paciente, 
 *conteo de pacientes,
 *ultumas 5 recetas, 
 *citas de un doctor, 
-*citas de un paciente obtener doctores dispono¡bles para una fecha, 
+*citas de un paciente obtener 
+*doctores dispono¡bles para una fecha, 
 obtener medicamentos de una receta, 
 *obtener el total de facturacion por mes*/
 
@@ -83,7 +61,8 @@ obtener medicamentos de una receta,
 // }
 
 /*------------- Edad Del Paciente ---------------*/
-// console.log(MantenimientoPacientes.obtenerEdadPaciente(2))
+
+console.log(paciente.obtenerEdadDelPaciente(1))
 
 /*----------------- total de facturacion por mes ------------------- */
 
--- src/interfaces/mantenimiento_interface/MantenimientoRecetas.ts
+++ src/interfaces/mantenimiento_interface/MantenimientoRecetas.ts
@@ -1,36 +1,23 @@
-import { readFileSync, writeFileSync } from 'fs'
-import { Receta, ActualizarReceta, CrearReceta } from '../receta.interface'
+// import * as fs from 'fs';
+// import { Receta } from '../receta.interface';
+// const direccionDeArchivo = 'data/recetas.json'
+// export class cargarRecetas {
+//     recetas: Receta[] = [];
 
-const direccionDeArchivo = 'data/recetas.json'
+//     constructor() {
+//         this.cargarRecetas();
+//     }
+//     private cargarRecetas(): void {
+//         try {
+//             const data = fs.readFileSync('./data/recetas.json', 'utf-8');
+//             this.recetas = JSON.parse(data);
+//         } catch (error) {
+//             console.error('Error al cargar las recetas:', error);
+//         }
+//     }
 
- const lectuaDeReceta = ():Receta []=>{
-    const archivo: string=readFileSync(direccionDeArchivo,'utf8')
-    if (archivo.trim()=== '' )return[]
-    return JSON.parse(archivo) as Receta[]
-}
-
-const CrearReceta =(CrearReceta : Receta) =>{
-    const productosactuales = lectuaDeReceta()
-    CrearReceta.recetaId = 3
-    productosactuales.push(CrearReceta as Receta)
-    writeFileSync(direccionDeArchivo, JSON.stringify(productosactuales))
-}
-const eliminarReceta= (recetaId: number)=>{
-    const RecetaActual = lectuaDeReceta()
-    const recetaFinal = RecetaActual.filter((Receta)=>Receta.recetaId!== recetaId)
-    writeFileSync(direccionDeArchivo, JSON.stringify(recetaFinal))
-}
-// const ActualizarReceta = (recetaId:number, actualizarreceta:ActualizarReceta )=>{
-//     const ActualizarReceta = lectuaDeReceta()
-//     const recetaconcambios = actualizarreceta.filter((Receta: { recetaId: number })=> Receta.recetaId == recetaId)[0]
-//     if (recetaconcambios.recetaId) recetaconcambios.recetaId= recetaconcambios.recetaId
-//     if (recetaconcambios.doctorId) recetaconcambios.doctorId= recetaconcambios.doctorId
-//     if (recetaconcambios.pacienteId) recetaconcambios.pacienteId= recetaconcambios.pacienteId
-//     if (recetaconcambios.fecha) recetaconcambios.fecha= recetaconcambios.fecha
-//     if (recetaconcambios.medicamento) recetaconcambios.medicamento= recetaconcambios.medicamento
-//     eliminarReceta(recetaId)
-//     const recetaFinal = lectuaDeReceta()
-//     recetaFinal.push(ActualizarReceta)
-//     writeFileSync(direccionDeArchivo, JSON.stringify(recetaFinal))
+// obtenerMedicamentosDeReceta(recetaId: number): string[] {
+//     const receta = this.recetas.find(receta => receta.id === recetaId);
+//     return receta ? receta.medicamentos.map(medicamento => medicamento.nombre) : [];
 // }
-export{lectuaDeReceta,CrearReceta,ActualizarReceta,eliminarReceta}
+// }
